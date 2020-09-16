944985419c24079282df0b00609bb1e2d8a84c04
JwowSquared <Jdwagner1@hotmail.com>
2020-09-16 10:28:09 +0000

    isdigits now does not allow push 98x, but correctly throws an error if the str is already pointing to a null byte

--- utilities_1.c
+++ utilities_1.c
@@ -10,10 +10,18 @@ int isdigits(char *str)
 {
 	int i = 0;
 
+	if (str[i] == '\0')
+		return (0);
+
 	if (str[i] == '-')
 		i++;
 
-	if (str[i] < '0' || str[i] > '9')
-		return (0);
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
+
 	return (1);
 }
